ff38f678b517719d9fe0e031e1f13c0408823be4
Demiana Ayman <133611525+Demi-ayman@users.noreply.github.com>
2025-12-05 09:02:36 +0200

    Delete TINYParserGUI directory

--- TINYParserGUI/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/deploy_TINYParserGUI_8447cd0ae5.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/CSE-Senior 2/Compiler/Parser/TINYParserGUI/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/TINYParserGUI-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "D:/CSE-Senior 2/Compiler/Parser/TINYParserGUI/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/TINYParserGUI.exe"
-    GENERATE_QT_CONF
-)
